b56dc3047d0f50f99dc338a11024a2e2b5f5ac2e
aliciacanales <canalesalicia06@gmail.com>
2024-01-30 16:18:35 -0800

    glarma

---
+++ scripts/growth_rate.R
@@ -0,0 +1,19 @@
+install.packages("growthrate")
+library(growthrate)
+
+##..........fit_easylinear..............
+## specfiy what time is
+## fit_easylinear algothrim uses subsets of h = n to find where the highest r <-growth rate. Uses time data and 
+##murmax is r 
+
+##..........fit_splines..............
+## uses polynomials. non parametric 
+
+##..........parametric nonlinear models........
+## FUN defines growth rate?
+## spar = is smoothness of data
+
+## can be used for dose-response curves. You would get different r values at different concentrations of the antibiotic
+
+##.......find r and k using bison data using three different models (exp,logistic, spline)............
+## code is in linus box
