0fc68634285d55b665dc569cbc654b8ef4551fd1
aliciacanales <canalesalicia06@gmail.com>
2024-02-08 16:44:19 -0800

    packages

---
+++ scripts/packages/fsa.R
@@ -0,0 +1,8 @@
+## Fisheries stock assessment
+## Weight-length Model 
+## Ws standard weight for fish of the same length.. usually already caluclated but not for every species. We would use wrAdd
+## for this you would need length, weight, and type of species
+
+## catch curve overviews
+## linear regression 
+## to calculate catch of fish at age t
---
+++ scripts/packages/survival.R
@@ -0,0 +1,11 @@
+library(survival)
+
+## assess time to event duration
+## predicts the influence of a variable
+## how long might cancer..
+## multiple models 
+## survival funciton: the prob of surving an event of interest
+## proportional hazards function 
+## looking at survival coefficient 
+## has it's own ggplot called ggsurvplot
+## KMC are best for quick decision making
