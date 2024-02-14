163d97d1bc41241c6be045b7df053895556e2948
aliciacanales <canalesalicia06@gmail.com>
2024-02-13 16:34:55 -0800

    recapr

---
+++ scripts/packages/recapr.R
@@ -0,0 +1,14 @@
+library(recapr)
+
+## two event mark recapture experiment. on marked species
+## power calculations
+## calculate recommended sampling size
+## type of conservation models -- used to estimate the numerical counts of something
+## estimates the current population size with a measure of uncertainty
+## can also be used to justify sample size
+## type of data -- number of animals marked on the first visit, second visit, and recaptured animals that were marked
+## common in fisheries
+
+## Example
+## get an estimate and put a bound on it
+## data intensive and has lots of uncertainty 
