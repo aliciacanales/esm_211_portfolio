a9691eaa9f82c7ffb2621670a823622f3781b776
aliciacanales <canalesalicia06@gmail.com>
2024-02-15 15:59:25 -0800

    adding the script of popdemo

---
+++ scripts/portfolios/popdemo.R
@@ -0,0 +1,13 @@
+library(popdemo)
+
+## demographic projection
+## birth rates
+## transition matrix
+## deterministic and stocsatic
+## project function
+## you can plot your projection object 
+
+## challenge assumes stability of transitions and mortality events
+
+## stochastic
+## you can tell it to bring up certain blocks of desired matrixes
